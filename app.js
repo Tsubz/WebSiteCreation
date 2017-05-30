f4a38133f5aa9e9296460e7930e57792b95ec80e
Tsubz <marcantoine.bazin@gmail.com>
2017-05-30 15:59:40 +0200

    Working Handlebars
    With one main layout with navbar, and "home"  handlebar for homepage
    content

---
+++ app.js
@@ -0,0 +1,39 @@
+/* Initialization */
+var express = require('express');
+var exphbs  = require('express-handlebars');
+
+var app = express()
+
+app.engine('handlebars', exphbs({defaultLayout: 'main'}));
+app.set('view engine', 'handlebars');
+app.use(express.static('files'));
+
+
+
+app.get('/', function (req, res) {
+    res.render('home',{userName:"Marc-Antoine"});
+});
+
+/*
+app.get('/game', function (req, res) {
+    res.render('game',{userName:"Marc-Antoine", gameHistory:hist});
+});
+
+app.get('/game/:playerchoice/', function (req, res) {
+    var serverPick = randomPick(),
+        playerPick = req.params.playerchoice;
+
+    res.render("game",{
+        serverChoice: serverPick,
+        playerChoice : playerPick,
+        gameResult:confrontation(serverPick,playerPick),
+        gameHistory:recordHist(serverPick,playerPick),
+    });
+    
+})
+*/
+
+/* Listen port */
+app.listen(3000, function () {
+  console.log('Our app listening on port 3000!')
+})
