fd9b8f1740e35f132e779a4e0409bcb8a7d9c08d
Tsubz <marcantoine.bazin@gmail.com>
2017-06-06 16:17:23 +0200

    branching with MongoDB
    Including MongoDB in the app.js

--- app.js
+++ app.js
@@ -8,12 +8,37 @@ app.engine('handlebars', exphbs({defaultLayout: 'main'}));
 app.set('view engine', 'handlebars');
 app.use(express.static('files'));
 
+var MongoClient = require('mongodb').MongoClient
+  , assert = require('assert');
 
+// Connection URL
+var url = 'mongodb://localhost:27017/';
 
+// Use connect method to connect to the server
+MongoClient.connect(url, function(err, db) {
+  assert.equal(null, err);
+  console.log("Connected successfully to DB");
+
+// HOME PAGE
 app.get('/', function (req, res) {
     res.render('home',{userName:"Marc-Antoine"});
 });
 
+
+/* Listen port */
+app.listen(3000, function () {
+  console.log('Our app listening on port 3000!')
+})
+
+
+});
+
+
+
+
+
+
+/**** Example for later ********/
 /*
 app.get('/game', function (req, res) {
     res.render('game',{userName:"Marc-Antoine", gameHistory:hist});
@@ -32,8 +57,3 @@ app.get('/game/:playerchoice/', function (req, res) {
     
 })
 */
-
-/* Listen port */
-app.listen(3000, function () {
-  console.log('Our app listening on port 3000!')
-})
