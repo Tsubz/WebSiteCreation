926121c81fd0b3665254f222e3b8d22729cddb9e
Marc-Antoine <marcantoine.bazin@gmail.com>
2017-06-09 12:13:14 +0200

    Adding Database / Dividing app.js - router

--- app.js
+++ app.js
@@ -1,6 +1,7 @@
 /* Initialization */
 var express = require('express');
 var exphbs  = require('express-handlebars');
+var bodyParser = require('body-parser');
 
 var app = express()
 
@@ -8,30 +9,62 @@ app.engine('handlebars', exphbs({defaultLayout: 'main'}));
 app.set('view engine', 'handlebars');
 app.use(express.static('files'));
 
+//Including stuff
+var routes = require('./routes/index');
+var users = require('./routes/users');
+
+app.use('/', routes);
+app.use('/users', users);
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: false }));
+
+//jquery
+/*var jquery = require("jsdom").env("", function(err, window) {
+    if (err) {
+        console.error(err);
+        return;
+    }
+ *
+    var $ = require("jquery");
+/*});*
+
+var jsdom = require('jsdom').jsdom;
+ var document = jsdom('<html></html>', {});
+ var window = document.defaultView;
+ var $ = require('jquery')(window); */
+
 var MongoClient = require('mongodb').MongoClient
   , assert = require('assert');
 
 // Connection URL
-var url = 'mongodb://localhost:27017/';
+var url = 'mongodb://localhost:27017/adeline';
 
-// Use connect method to connect to the server
+/* //Mongo db accessible
+app.use(function(req,res,next){
+    req.db = db;
+    next();
+}); 
+*/
+
+/* Use connect method to connect to the server
 MongoClient.connect(url, function(err, db) {
   assert.equal(null, err);
   console.log("Connected successfully to DB");
+  // End of MongoDb Connection
+ //db.close();
+});*/
 
-// HOME PAGE
-app.get('/', function (req, res) {
-    res.render('home',{userName:"Marc-Antoine"});
+/*
+// ADD TEST PAGE
+app.get('/add', function (req, res) {
+    res.render('add',{userName:"Marc-Antoine"});
 });
-
+*/
 
 /* Listen port */
 app.listen(3000, function () {
   console.log('Our app listening on port 3000!')
 })
-
-
-});
 
 
 
---
+++ routes/users.js
@@ -0,0 +1,55 @@
+var express = require('express');
+var router = express.Router();
+
+var MongoClient = require('mongodb').MongoClient
+  , assert = require('assert');
+
+// Connection URL
+var url = 'mongodb://localhost:27017/adeline';
+
+//READ, UPDATE, DELETE users 
+router.get('/users', function(req, res) {
+      res.render('users', {Title:"users"});
+});
+
+
+/* INSERT user */
+router.post('/insert_user', function(req, res) {
+ 
+      // Get POST values
+      var userName = req.body.name;
+      var userSurname = req.surname;
+      console.log('POST VALUES: ' + userName + ' ' + userSurname);
+
+      // Fetch from 'users' collection
+      MongoClient.connect(url, function(err, db) {
+            assert.equal(null, err);
+            console.log("Connected to add element");
+
+            var tryCo = db.collection("try");
+
+            tryCo.insert({
+                  'username' : userName,
+                  'usersurname' : userSurname
+            }, function(err, doc) {
+                  if(err) res.send('Problem occured when inserting in users collection');
+                  else {
+                        console.log("Inserted");
+                        res.location('users');
+                        res.redirect('/users');
+                  }
+            });
+
+            // End of MongoDb Connection
+      db.close();
+      });      
+});
+
+
+/* NEW user */
+router.get('/new', function(req, res) {
+      res.render('user-new');
+});
+
+
+module.exports = router;
