c2ef4b939bbc9c467e23471556c3e07387772efa
Marc-Antoine <marcantoine.bazin@gmail.com>
2017-06-13 20:24:08 +0200

    beautify 2

--- app.js
+++ app.js
@@ -1,22 +1,26 @@
 /* Initialization */
 var express = require('express');
-var exphbs  = require('express-handlebars');
+var exphbs = require('express-handlebars');
 var bodyParser = require('body-parser');
 var formidable = require('formidable');
 var fs = require('fs');
-var multer  = require('multer')
-var upload = multer({ dest: 'files/img' })
+var multer = require('multer');
+var upload = multer({
+  dest: 'files/img'
+});
 
-var app = express()
+var app = express();
 
-app.engine('handlebars', exphbs({defaultLayout: 'main'}));
+app.engine('handlebars', exphbs({
+  defaultLayout: 'main'
+}));
 app.set('view engine', 'handlebars');
 app.use(express.static('files'));
 
 //MongoDB
-var MongoClient = require('mongodb').MongoClient
-  , assert = require('assert');
- // Connection URL
+var MongoClient = require('mongodb').MongoClient,
+  assert = require('assert');
+// Connection URL
 var url = 'mongodb://localhost:27017/adeline';
 
 //Including Routes
@@ -24,7 +28,9 @@ var routes = require('./routes/index');
 var user = require('./routes/user');
 
 app.use(bodyParser.json());
-app.use(bodyParser.urlencoded({ extended: true }));
+app.use(bodyParser.urlencoded({
+  extended: true
+}));
 
 app.use('/', routes);
 app.use('/user', user);
@@ -61,13 +67,15 @@ MongoClient.connect(url, function(err, db) {
 
 
 // ADD TEST PAGE
-app.get('/user', function (req, res) {
-    res.render('user',{userName:"Marc-Antoine"});
+app.get('/user', function(req, res) {
+  res.render('user', {
+    userName: "Marc-Antoine"
+  });
 });
 
 
 /* Listen port */
-app.listen(1234, function () {
+app.listen(1234, function() {
   console.log('Our app listening on port 1234')
 });
 
