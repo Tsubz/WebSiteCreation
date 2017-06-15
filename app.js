39680c81762362ff0818209c33c4db4d1370ced4
Marc-Antoine <marcantoine.bazin@gmail.com>
2017-06-15 18:22:31 +0200

    Routing twist

--- app.js
+++ app.js
@@ -3,9 +3,10 @@ var express = require('express');
 var exphbs = require('express-handlebars');
 var multer = require('multer');
 var assert = require('assert');
-//var upload = multer({ dest: 'files/img' });
-
 var app = express();
+
+var router = express.Router();
+app.use(router);
 
 app.engine('handlebars', exphbs({
   defaultLayout: 'main'
@@ -23,46 +24,17 @@ var url = 'mongodb://localhost:27017/adeline';
 var routes = require('./routes/index');
 var user = require('./routes/user');
 
-/*app.use(bodyParser.json());
-app.use(bodyParser.urlencoded({
-  extended: true
-}));*/
-
 app.use('/', routes);
 app.use('/user', user);
 
-
-// ADD TEST PAGE
-app.get('/user', function(req, res) {
-        res.render('user', {
-          userName: "Marc-Antoine"
-        });
-});
-
+  // TEST PAGE
+  router.get('/user', function(req, res) {
+    res.render('user', {
+      title1: 'User Page'
+    });
+  });
 
 /* Listen port */
 app.listen(1234, function() {
   console.log('Our app listening on port 1234');
 });
-
-
-
-/**** Example for later ********/
-/*
-app.get('/game', function (req, res) {
-    res.render('game',{userName:"Marc-Antoine", gameHistory:hist});
-});
-
-app.get('/game/:playerchoice/', function (req, res) {
-    var serverPick = randomPick(),
-        playerPick = req.params.playerchoice;
-
-    res.render("game",{
-        serverChoice: serverPick,
-        playerChoice : playerPick,
-        gameResult:confrontation(serverPick,playerPick),
-        gameHistory:recordHist(serverPick,playerPick),
-    });
-
-})
-*/
--- routes/user.js
+++ routes/user.js
@@ -1,30 +1,35 @@
 var express = require('express');
 var multer = require('multer');
 var assert = require('assert');
+var router = express.Router();
 
 // Multer Storage Info //
 var storage = multer.diskStorage({
-  destination: function (req, file, cb) {
+  destination: function(req, file, cb) {
     cb(null, 'files/img');
   },
-  filename: function (req, file, cb) {
+  filename: function(req, file, cb) {
     cb(null, file.originalname);
   }
 });
+var upload = multer({storage: storage});
 
-var upload = multer({ storage: storage });
-
-var router = express.Router();
-
-var MongoClient = require('mongodb').MongoClient,
-  assert = require('assert');
 // Connection URL
 var url = 'mongodb://localhost:27017/adeline';
+var MongoClient = require('mongodb').MongoClient,
+  assert = require('assert');
+
+  // TEST PAGE
+  router.get('/user', function(req, res) {
+    res.render('user', {
+      title1: 'User Page'
+    });
+  });
 
 /* INSERT user */
-router.post('/upload_img', upload.single('exInputFile'), function(req,res){
+router.post('/upload_img', upload.single('exInputFile'), function(req, res) {
 
-	console.log(req.file);
+  console.log(req.file);
 
   var file = req.file.originalname;
 
@@ -51,7 +56,7 @@ router.post('/upload_img', upload.single('exInputFile'), function(req,res){
 });
 
 router.get('/get-data', function(req, res, next) {
-  var img_data= [];
+  var img_data = [];
 
   MongoClient.connect(url, function(err, db) {
     assert.equal(null, err);
@@ -59,17 +64,15 @@ router.get('/get-data', function(req, res, next) {
 
     var cursor = db.collection('try').find();
     cursor.forEach(function(doc, err) {
-        assert.equal(null, err);
-
-          img_data.push(doc);
-
+      assert.equal(null, err);
+      img_data.push(doc);
     }, function() {
-        db.close();
-        console.log("DB Closed");
-        res.render('user', {
-          userName: "Marc-Antoine",
-          imgdata: img_data,
-        });
+      db.close();
+      console.log("DB Closed");
+      res.render('user', {
+        userName: "Marc-Antoine",
+        imgdata: img_data,
+      });
     });
   });
 });
