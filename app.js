5426b169237c14ac358d0d6fc07d1cefb70704aa
Marc-Antoine <marcantoine.bazin@gmail.com>
2017-06-15 18:43:31 +0200

    Working upload / Display WIP

--- app.js
+++ app.js
@@ -1,15 +1,12 @@
 /* Initialization */
 var express = require('express');
 var exphbs = require('express-handlebars');
-var bodyParser = require('body-parser');
-var formidable = require('formidable');
-var fs = require('fs');
 var multer = require('multer');
-var upload = multer({
-  dest: 'files/img'
-});
-
+var assert = require('assert');
 var app = express();
+
+var router = express.Router();
+app.use(router);
 
 app.engine('handlebars', exphbs({
   defaultLayout: 'main'
@@ -27,76 +24,17 @@ var url = 'mongodb://localhost:27017/adeline';
 var routes = require('./routes/index');
 var user = require('./routes/user');
 
-app.use(bodyParser.json());
-app.use(bodyParser.urlencoded({
-  extended: true
-}));
-
 app.use('/', routes);
 app.use('/user', user);
 
-//jquery
-/*var jquery = require("jsdom").env("", function(err, window) {
-    if (err) {
-        console.error(err);
-        return;
-    }
- *
-    var $ = require("jquery");
-/*});*
-
-var jsdom = require('jsdom').jsdom;
- var document = jsdom('<html></html>', {});
- var window = document.defaultView;
- var $ = require('jquery')(window); */
-
-/* //Mongo db accessible
-app.use(function(req,res,next){
-    req.db = db;
-    next();
-});
-*/
-
-/* Use connect method to connect to the server
-MongoClient.connect(url, function(err, db) {
-  assert.equal(null, err);
-  console.log("Connected successfully to DB");
-  // End of MongoDb Connection
- //db.close();
-});*/
-
-
-// ADD TEST PAGE
-app.get('/user', function(req, res) {
-  res.render('user', {
-    userName: "Marc-Antoine"
+  // TEST PAGE
+  router.get('/user', function(req, res) {
+    res.render('user', {
+      title1: 'User Page'
+    });
   });
-});
-
 
 /* Listen port */
 app.listen(1234, function() {
   console.log('Our app listening on port 1234');
 });
-
-
-
-/**** Example for later ********/
-/*
-app.get('/game', function (req, res) {
-    res.render('game',{userName:"Marc-Antoine", gameHistory:hist});
-});
-
-app.get('/game/:playerchoice/', function (req, res) {
-    var serverPick = randomPick(),
-        playerPick = req.params.playerchoice;
-
-    res.render("game",{
-        serverChoice: serverPick,
-        playerChoice : playerPick,
-        gameResult:confrontation(serverPick,playerPick),
-        gameHistory:recordHist(serverPick,playerPick),
-    });
-
-})
-*/
--- routes/user.js
+++ routes/user.js
@@ -1,50 +1,39 @@
 var express = require('express');
-var formidable = require('formidable');
-var fs = require('fs');
-
+var multer = require('multer');
+var assert = require('assert');
 var router = express.Router();
 
-var MongoClient = require('mongodb').MongoClient,
-  assert = require('assert');
+// Multer Storage Info //
+var storage = multer.diskStorage({
+  destination: function(req, file, cb) {
+    cb(null, 'files/img');
+  },
+  filename: function(req, file, cb) {
+    cb(null, file.originalname);
+  }
+});
+var upload = multer({storage: storage});
 
 // Connection URL
 var url = 'mongodb://localhost:27017/adeline';
+var MongoClient = require('mongodb').MongoClient,
+  assert = require('assert');
 
-//// https://www.npmjs.com/package/multer#usage ////
-
-/* INSERT user */
-router.post('/insert_user', function(req, res) {
-
-  // Get POST values
-  var userName = req.body.name;
-  var userSurname = req.body.surname;
-
-  console.log('POST VALUES: ' + userName + ' ' + userSurname);
-
-  var form = new formidable.IncomingForm();
-
-  form.parse(req, function(err, fields, files) {
-    var oldpath = files.exInputFile.path;
-    var newpath = 'files/img/' + files.exInputFile.name;
-    fs.readFile(oldpath, function(err, data) {
-      if (err) throw err;
-      console.log('File read!');
-
-      // Write the file
-      fs.writeFile(newpath, data, function(err) {
-        if (err) throw err;
-        console.log('File written!');
-      });
-
-      // Delete the file
-      fs.unlink(oldpath, function(err) {
-        if (err) throw err;
-        console.log('File deleted!');
-      });
+  // TEST PAGE
+  router.get('/user', function(req, res) {
+    res.render('user', {
+      title1: 'User Page'
     });
   });
 
-  // Fetch from 'users' collection
+/* INSERT user */
+router.post('/upload_img', upload.single('exInputFile'), function(req, res) {
+
+  console.log(req.file);
+
+  var file = req.file.originalname;
+
+  // Inserting into database 'try' collection
   MongoClient.connect(url, function(err, db) {
     assert.equal(null, err);
     console.log("Connected to add element");
@@ -52,26 +41,40 @@ router.post('/insert_user', function(req, res) {
     var tryCo = db.collection("try");
 
     tryCo.insert({
-      'username': userName,
-      'usersurname': userSurname
+      'filename': file,
     }, function(err, doc) {
-      if (err) res.send('Problem occured when inserting in users collection');
+      if (err) res.send('Problem occured when inserting in try collection');
       else {
         console.log("Inserted");
         res.location('user');
         res.redirect('/user');
       }
     });
-
     // End of MongoDb Connection
     db.close();
   });
 });
 
+router.get('/get-data', function(req, res, next) {
+  var img_data = [];
 
-/* NEW user */
-router.get('/new', function(req, res) {
-  res.render('user-new');
+  MongoClient.connect(url, function(err, db) {
+    assert.equal(null, err);
+    console.log("Connected to DB");
+
+    var cursor = db.collection('try').find();
+    cursor.forEach(function(doc, err) {
+      assert.equal(null, err);
+      img_data.push(doc);
+    }, function() {
+      db.close();
+      console.log("DB Closed");
+      res.render('user', {
+        userName: "Marc-Antoine",
+        imgdata: img_data,
+      });
+    });
+  });
 });
 
 
