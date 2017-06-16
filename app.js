a2e60201bc8c56e9930e0f7da12f2429955497f5
Marc-Antoine <marcantoine.bazin@gmail.com>
2017-06-16 15:07:52 +0200

    Can display Images / user renamed admin

--- app.js
+++ app.js
@@ -3,6 +3,7 @@ var express = require('express');
 var exphbs = require('express-handlebars');
 var multer = require('multer');
 var assert = require('assert');
+var path = require('path');
 var app = express();
 
 var router = express.Router();
@@ -12,7 +13,7 @@ app.engine('handlebars', exphbs({
   defaultLayout: 'main'
 }));
 app.set('view engine', 'handlebars');
-app.use(express.static('files'));
+app.use(express.static(path.join(__dirname, 'files')));
 
 //MongoDB
 var MongoClient = require('mongodb').MongoClient,
@@ -22,17 +23,10 @@ var url = 'mongodb://localhost:27017/adeline';
 
 //Including Routes
 var routes = require('./routes/index');
-var user = require('./routes/user');
+var admin = require('./routes/admin');
 
 app.use('/', routes);
-app.use('/user', user);
-
-  // TEST PAGE
-  router.get('/user', function(req, res) {
-    res.render('user', {
-      title1: 'User Page'
-    });
-  });
+app.use('/admin', admin);
 
 /* Listen port */
 app.listen(1234, function() {
--- routes/admin.js
+++ routes/admin.js
@@ -2,6 +2,7 @@ var express = require('express');
 var multer = require('multer');
 var assert = require('assert');
 var router = express.Router();
+var img_data = [];
 
 // Multer Storage Info //
 var storage = multer.diskStorage({
@@ -19,19 +20,34 @@ var url = 'mongodb://localhost:27017/adeline';
 var MongoClient = require('mongodb').MongoClient,
   assert = require('assert');
 
-  // TEST PAGE
-  router.get('/user', function(req, res) {
-    res.render('user', {
-      title1: 'User Page'
+  // ADMIN PAGE
+  router.get('/', function(req, res) {
+    MongoClient.connect(url, function(err, db) {
+      assert.equal(null, err);
+      console.log("Connected to DB");
+
+      var cursor = db.collection('try').find();
+      cursor.forEach(function(doc, err) {
+        assert.equal(null, err);
+        img_data.push(doc);
+      }, function() {
+        db.close();
+        console.log("DB Closed");
+
+    res.render('admin', {
+      title1: 'admin Page',
+      imgdata: img_data,
+      });
     });
   });
+});
 
 /* INSERT user */
 router.post('/upload_img', upload.single('exInputFile'), function(req, res) {
 
   console.log(req.file);
 
-  var file = req.file.originalname;
+  var file = '/img/'+req.file.originalname;
 
   // Inserting into database 'try' collection
   MongoClient.connect(url, function(err, db) {
@@ -46,34 +62,12 @@ router.post('/upload_img', upload.single('exInputFile'), function(req, res) {
       if (err) res.send('Problem occured when inserting in try collection');
       else {
         console.log("Inserted");
-        res.location('user');
-        res.redirect('/user');
+        res.location('admin');
+        res.redirect('/admin');
       }
     });
     // End of MongoDb Connection
     db.close();
-  });
-});
-
-router.get('/get-data', function(req, res, next) {
-  var img_data = [];
-
-  MongoClient.connect(url, function(err, db) {
-    assert.equal(null, err);
-    console.log("Connected to DB");
-
-    var cursor = db.collection('try').find();
-    cursor.forEach(function(doc, err) {
-      assert.equal(null, err);
-      img_data.push(doc);
-    }, function() {
-      db.close();
-      console.log("DB Closed");
-      res.render('user', {
-        userName: "Marc-Antoine",
-        imgdata: img_data,
-      });
-    });
   });
 });
 
