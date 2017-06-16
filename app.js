1e4cd754b6d587d1268b8b636104495020869f17
JunWakamatsu <b00720597@essec.edu>
2017-06-16 17:33:45 +0200

    carousel_erasegray

--- app.js
+++ app.js
@@ -1,40 +1,20 @@
 /* Initialization */
 var express = require('express');
-var exphbs = require('express-handlebars');
-var multer = require('multer');
-var assert = require('assert');
-var app = express();
+var exphbs  = require('express-handlebars');
 
-var router = express.Router();
-app.use(router);
+var app = express()
 
-app.engine('handlebars', exphbs({
-  defaultLayout: 'main'
-}));
+app.engine('handlebars', exphbs({defaultLayout: 'main'}));
 app.set('view engine', 'handlebars');
 app.use(express.static('files'));
 
-//MongoDB
-var MongoClient = require('mongodb').MongoClient,
-  assert = require('assert');
-// Connection URL
-var url = 'mongodb://localhost:27017/adeline';
 
-//Including Routes
-var routes = require('./routes/index');
-var user = require('./routes/user');
 
-app.use('/', routes);
-app.use('/user', user);
-
-  // TEST PAGE
-  router.get('/user', function(req, res) {
-    res.render('user', {
-      title1: 'User Page'
-    });
-  });
+app.get('/', function (req, res) {
+    res.render('home',{userName:"Marc-Antoine"});
+});
 
 /* Listen port */
-app.listen(1234, function() {
-  console.log('Our app listening on port 1234');
-});
+app.listen(1234, function () {
+  console.log('Our app listening on port 1234!')
+})
