08ef39c25b3aef583c0b86e54166078d1d99a1d2
Marc-Antoine <marcantoine.bazin@gmail.com>
2017-06-17 16:09:34 +0200

    Simplify database connection / async.parallel to get data

--- routes/admin.js
+++ routes/admin.js
@@ -2,6 +2,8 @@ var express = require('express');
 var multer = require('multer');
 var assert = require('assert');
 var router = express.Router();
+var async = require("async");
+var db = require('../helpers/db');
 
 // Multer Storage Info //
 var storage = multer.diskStorage({
@@ -16,72 +18,59 @@ var upload = multer({
   storage: storage
 });
 
-// Connection URL
-var url = 'mongodb://localhost:27017/adeline';
-var MongoClient = require('mongodb').MongoClient,
-  assert = require('assert');
+//var MongoClient = require('mongodb').MongoClient;
 
 // ADMIN PAGE
 router.get('/', function(req, res) {
   var img_data = [];
   var pjcts = [];
-  MongoClient.connect(url, function(err, db) {
-    assert.equal(null, err);
-    console.log("Connected to DB");
 
-    var img_cursor = db.collection('imgs').find();
-    var pj_cursor = db.collection('project').find();
+  var img_cursor = db.get().collection('imgs').find();
+  var pj_cursor = db.get().collection('project').find();
 
-    img_cursor.forEach(function(doc, err) {
-      assert.equal(null, err);
-      img_data.push(doc);
-    }, function() { //Callback function
-      pj_cursor.forEach(function(doc, err) {
-        assert.equal(null, err);
-        pjcts.push(doc);
-      }, function() { //Second Callback inside first Callback
-
-        db.close();
-        console.log("DB Closed");
-
-        res.render('admin', {
-          title1: 'admin Page',
-          imgdata: img_data,
-          pjdata: pjcts,
-        });
+  async.parallel(
+    [
+      function(cb) {
+        img_cursor.forEach(function(doc, err) {
+          assert.equal(null, err);
+          img_data.push(doc);
+        }, cb);
+      },
+      function(cb) {
+        pj_cursor.forEach(function(doc, err) {
+          assert.equal(null, err);
+          pjcts.push(doc);
+        }, cb);
+      }
+    ],
+    function(err, results) {
+      res.render('admin', {
+        title1: 'admin Page',
+        imgdata: img_data,
+        pjdata: pjcts,
       });
-    });
-  });
+    }
+  );
 });
 
 /* Upload Image -- single('exInputFile')*/
 router.post('/upload_img', upload.any(), function(req, res) {
 
   // Inserting into database 'Imgs' collection
-  MongoClient.connect(url, function(err, db) {
-    assert.equal(null, err);
-    console.log("Connected to add element");
-    var addImg = db.collection("imgs");
 
-    var file;
-    for (var i=0; i < req.files.length; i++) {
-      console.log(req.files[i].originalname);
-      file = '/img/' + req.files[i].originalname;
+  var addImg = db.get().collection("imgs");
 
-      addImg.insert({
-        'filename': file,
-      }/*, function(err, doc) {
-        if (err) res.send('Problem occured when inserting in imgs collection');
-        else {
-          console.log("Inserted"); }
-        }*/);
-    }
-    res.location('admin');
-    res.redirect('/admin');
-    // End of MongoDb Connection
-    db.close();
-    //console.log("upload DB Closed");
-  });
+  var file;
+  for (var i = 0; i < req.files.length; i++) {
+    console.log(req.files[i].originalname);
+    file = '/img/' + req.files[i].originalname;
+
+    addImg.insert({
+      'filename': file,
+    });
+  }
+  res.location('admin');
+  res.redirect('/admin');
 });
 
 /* Create Project */
@@ -94,25 +83,18 @@ router.post('/addproject', function(req, res) {
   console.log('POST VALUES: ' + projectname + ' ' + projectdesc);
 
   // Inserting into database 'Project' collection
-  MongoClient.connect(url, function(err, db) {
-    assert.equal(null, err);
-    console.log("Connected to add element");
+  var addProject = db.get().collection("project");
 
-    var addProject = db.collection("project");
-
-    addProject.insert({
-      'projectname': projectname,
-      'projectdesc': projectdesc,
-    }, function(err, doc) {
-      if (err) res.send('Problem occured when inserting in project collection');
-      else {
-        console.log("Inserted");
-        res.location('admin');
-        res.redirect('/admin');
-      }
-    });
-    // End of MongoDb Connection
-    db.close();
+  addProject.insert({
+    'projectname': projectname,
+    'projectdesc': projectdesc,
+  }, function(err, doc) {
+    if (err) res.send('Problem occured when inserting in project collection');
+    else {
+      console.log("Inserted");
+      res.location('admin');
+      res.redirect('/admin');
+    }
   });
 });
 
@@ -127,25 +109,18 @@ router.post('/checkboxes', function(req, res) {
   console.log('POST VALUES: ' + checkimg);
 
   // Inserting into database 'img_pj' collection
-  MongoClient.connect(url, function(err, db) {
-    assert.equal(null, err);
-    console.log("Connected to add element");
+  var img_pj = db.get().collection("img_pj");
 
-    var img_pj = db.collection("img_pj");
-
-    img_pj.insert({
-      'projectname': projectname,
-      'projectimgs': checkimg,
-    }, function(err, doc) {
-      if (err) res.send('Problem occured when inserting in img_pj collection');
-      else {
-        console.log("Inserted");
-        res.location('admin');
-        res.redirect('/admin');
-      }
-    });
-    // End of MongoDb Connection
-    db.close();
+  img_pj.insert({
+    'projectname': projectname,
+    'projectimgs': checkimg,
+  }, function(err, doc) {
+    if (err) res.send('Problem occured when inserting in img_pj collection');
+    else {
+      console.log("Inserted");
+      res.location('admin');
+      res.redirect('/admin');
+    }
   });
 });
 
