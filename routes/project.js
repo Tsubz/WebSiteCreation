0f2ea2e366326c6d34d19d5230ea13477b2490bd
Marc-Antoine <marcantoine.bazin@gmail.com>
2017-06-18 11:54:35 +0200

    Can display dynamic project info
    Need definitive styling for project page to complete

--- routes/project.js
+++ routes/project.js
@@ -8,9 +8,14 @@ var router = express.Router();
 router.get('/:projectname/', function(req, res) {
   var pjcts = [];
   var pjcts_img = [];
+  var pjct_current = [];
+  var pjct_desc = '';
   var projectname = req.params.projectname;
 
   var pj_cursor = db.get().collection('project').find();
+  var pj_desc_cursor = db.get().collection('project').find({
+    'projectname': projectname
+  });
   var pj_img_cursor = db.get().collection('img_pj').find({
     'projectname': projectname
   });
@@ -28,6 +33,12 @@ router.get('/:projectname/', function(req, res) {
           assert.equal(null, err);
           pjcts_img.push(doc);
         }, cb);
+      },
+      function(cb) {
+        pj_desc_cursor.forEach(function(doc, err) {
+          assert.equal(null, err);
+          pjct_current.push(doc);
+        }, cb);
       }
     ],
     function(err, results) {
@@ -36,17 +47,23 @@ router.get('/:projectname/', function(req, res) {
           function(cb) {
             var i = pjcts_img.indexOf([]);
             if (i != -1) {
-              checkimg.splice(i, 1);
+              pjcts_img.splice(i, 1);
             }
-            console.log(pjcts_img);
+            cb();
+          },
+          function(cb) {
+            pjct_desc = pjct_current[0].projectdesc;
+            console.log(pjct_desc);
             cb();
           }
         ],
         function(err, results) {
           res.render('project', {
             pagetitle: 'Projects',
+            projectname: projectname,
             pjdata: pjcts,
-            featuredimg: pjcts_img,
+            images: pjcts_img,
+            pjct_desc: pjct_desc,
           });
         }
       );
